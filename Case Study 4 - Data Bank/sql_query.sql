5988b1acdb3d165e9b7e046808853e7901e40ef7
Jaime M. Shaker <eyeweld@gmail.com>
2022-08-02 21:26:52 -0500

    Added section B question #1-#3

--- Case Study 4 - Data Bank/sql_query.sql
+++ Case Study 4 - Data Bank/sql_query.sql
@@ -154,9 +154,153 @@ Asia       |  14.0|           23.0|           27.0|
 Australia  |  16.0|           23.0|           28.0|
 Europe     |  15.0|           24.0|           28.0|
         
+
+-- B. Customer Transactions
+
+-- 1. What is the unique count and total amount for each transaction type? 
+
+SELECT
+	DISTINCT txn_type AS transaction_type,
+	count(*) AS transaction_count,
+	sum(txn_amount) AS total_transactions
+FROM
+	customer_transactions
+GROUP BY 
+	txn_type
+
+-- OR
         
-        
-        
-        
-        
+SELECT 
+	DISTINCT txn_type AS transaction_type,
+	count(
+		CASE
+			WHEN txn_type = 'purchase' THEN 1
+			WHEN txn_type = 'withdrawal' THEN 1
+			WHEN txn_type = 'deposit' THEN 1
+			ELSE null
+		END 
+	) AS transaction_count,
+	sum(
+		CASE
+			WHEN txn_type = 'purchase' THEN txn_amount
+			WHEN txn_type = 'withdrawal' THEN txn_amount
+			WHEN txn_type = 'deposit' THEN txn_amount
+			ELSE 0	
+		END 
+	) AS total_transactions
+FROM
+	customer_transactions
+GROUP BY transaction_type
+	
+-- Results:
+
+transaction_type|transaction_count|total_transactions|
+----------------+-----------------+------------------+
+deposit         |             2671|           1359168|
+purchase        |             1617|            806537|
+withdrawal      |             1580|            793003|
+
+-- 2. What is the average total historical deposit counts and amounts for all customers?
+
+SELECT
+	round(avg(deposits_count)) AS avg_deposit_count,
+	round(avg(total_deposit_amount)) AS avg_deposit_amount
+from
+	(SELECT
+		customer_id,
+		count(*) AS deposits_count,
+		avg(txn_amount) AS total_deposit_amount
+	FROM customer_transactions
+	WHERE txn_type = 'deposit'
+	GROUP BY
+		customer_id) AS tmp
+
+-- Results:
+		
+avg_deposit_count|avg_deposit_amount|
+-----------------+------------------+
+                5|               509|
+
+-- 3. For each month - how many Data Bank customers make more than 1 deposit and either 1 purchase or 1 withdrawal in a single month?
+
+SELECT
+	transaction_month,
+	count(customer_id) AS customer_count
+from
+	(SELECT
+		DISTINCT customer_id,
+		date_part('month', txn_date) AS transaction_month,
+		count(
+			CASE
+				WHEN txn_type = 'purchase' THEN 1
+				ELSE null
+			END  
+		) AS purchase_count,
+		count(
+			CASE
+				WHEN txn_type = 'withdrawal' THEN 1
+				ELSE null
+			END  
+		) AS withdrawal_count,
+		count(
+			CASE
+				WHEN txn_type = 'deposit' THEN 1
+				ELSE null
+			END  
+		) AS deposit_count
+	FROM customer_transactions
+	GROUP BY
+		customer_id,
+		transaction_month) AS tmp
+WHERE deposit_count > 1
+AND (purchase_count >= 1
+OR withdrawal_count >= 1)
+GROUP BY transaction_month
+ORDER BY transaction_month
+
+-- Results:
+
+transaction_month|customer_count|
+-----------------+--------------+
+              1.0|           168|
+              2.0|           181|
+              3.0|           192|
+              4.0|            70|
+              
+-- 4. What is the closing balance for each customer at the end of the month?
+
+WITH deposits AS (
+	SELECT
+		DISTINCT customer_id,
+		date_part('month', txn_date) AS month,
+		sum(txn_amount) AS total_deposits
+	FROM
+		customer_transactions
+	WHERE txn_type = 'deposit'
+	GROUP BY 
+		customer_id,
+		month
+),
+withdrawals AS (
+	SELECT
+		DISTINCT customer_id,
+		date_part('month', txn_date) AS month,
+		sum(txn_amount) AS total_withdrawals
+	FROM
+		customer_transactions
+	WHERE txn_type = 'withdrawal'
+	OR txn_type = 'purchase'
+	GROUP BY 
+		customer_id,
+		month
+)
+
+SELECT
+	d.customer_id,
+	d.month
+FROM deposits AS d
+ORDER BY 
+	d.customer_id,
+	d.month
+
     
