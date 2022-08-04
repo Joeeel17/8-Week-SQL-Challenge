8ca407e37e376623e97694bab112e92bc02fe8c5
Jaime M. Shaker <eyeweld@gmail.com>
2022-08-03 20:56:54 -0500

    Added question #4

--- Case Study 4 - Data Bank/sql_query.sql
+++ Case Study 4 - Data Bank/sql_query.sql
@@ -230,26 +230,26 @@ avg_deposit_count|avg_deposit_amount|
 -- 3. For each month - how many Data Bank customers make more than 1 deposit and either 1 purchase or 1 withdrawal in a single month?
 
 SELECT
-	transaction_month,
+	current_month,
 	count(customer_id) AS customer_count
 FROM
 	(
 	SELECT
 		DISTINCT customer_id,
-		date_part('month', txn_date) AS transaction_month,
-		count(
+		to_char(txn_date, 'Month') AS current_month,
+		sum(
 			CASE
 				WHEN txn_type = 'purchase' THEN 1
 				ELSE NULL
 			END  
 		) AS purchase_count,
-		count(
+		sum(
 			CASE
 				WHEN txn_type = 'withdrawal' THEN 1
 				ELSE NULL
 			END  
 		) AS withdrawal_count,
-		count(
+		sum(
 			CASE
 				WHEN txn_type = 'deposit' THEN 1
 				ELSE NULL
@@ -259,59 +259,84 @@ FROM
 		customer_transactions
 	GROUP BY
 		customer_id,
-		transaction_month) AS tmp
+		current_month) AS tmp
 WHERE
 	deposit_count > 1
-	AND (purchase_count >= 1
-		OR withdrawal_count >= 1)
+	AND (purchase_count = 1
+		OR withdrawal_count = 1)
 GROUP BY
-	transaction_month
+	current_month
 ORDER BY
-	transaction_month
+	to_date(current_month, 'Month');
 
 -- Results:
 
-transaction_month|customer_count|
------------------+--------------+
-              1.0|           168|
-              2.0|           181|
-              3.0|           192|
-              4.0|            70|
-              
--- 4. What is the closing balance for each customer at the end of the month?
+current_month|customer_count|
+-------------+--------------+
+January      |           115|
+February     |           108|
+March        |           113|
+April        |            50|
 
-/*WITH deposits AS (
-	SELECT
-		DISTINCT customer_id,
-		date_part('month', txn_date) AS month,
-		sum(txn_amount) AS total_deposits
-	FROM
-		customer_transactions
-	WHERE txn_type = 'deposit'
-	GROUP BY 
-		customer_id,
-		month
-),
-withdrawals AS (
-	SELECT
-		DISTINCT customer_id,
-		date_part('month', txn_date) AS month,
-		sum(txn_amount) AS total_withdrawals
-	FROM
-		customer_transactions
-	WHERE txn_type = 'withdrawal'
-	OR txn_type = 'purchase'
-	GROUP BY 
-		customer_id,
-		month
-)
+
+-- 4. What is the closing balance for each customer at the end of the month?
+-- Limit 15 as a sample
 
 SELECT
-	d.customer_id,
-	d.month
-FROM deposits AS d
-ORDER BY 
-	d.customer_id,
-	d.month*/
+	customer_id,
+	current_month,
+	transaction_amount,
+	sum(transaction_amount) OVER (PARTITION BY customer_id ORDER BY current_month ROWS BETWEEN UNBOUNDED preceding AND CURRENT ROW) AS closing_balance
+from
+	(SELECT
+		customer_id,
+		to_char(txn_date, 'Month') AS current_month,
+		txn_type,
+		sum(
+			CASE 
+				WHEN txn_type = 'deposit' THEN txn_amount
+				ELSE -txn_amount
+			END
+		) AS transaction_amount
+	FROM customer_transactions
+	GROUP BY
+		customer_id,
+		current_month,
+		txn_type
+	ORDER by customer_id) AS tmp
+ORDER BY
+	customer_id, to_date(current_month, 'Month')
+LIMIT 15
+  
+-- Results:
 
-    
+customer_id|current_month|transaction_amount|closing_balance|
+-----------+-------------+------------------+---------------+
+          1|January      |               312|            312|
+          1|March        |             -1276|           -964|
+          1|March        |               324|           -640|
+          2|January      |               549|            549|
+          2|March        |                61|            610|
+          3|January      |               144|           -328|
+          3|February     |              -965|           -472|
+          3|March        |              -401|           -729|
+          3|April        |               493|            493|
+          4|January      |               848|            848|
+          4|March        |              -193|            655|
+          5|January      |              -826|          -1316|
+          5|January      |              1780|            464|
+          5|March        |              1130|          -2413|
+          5|March        |             -1486|          -1022|
+
+
+-- 5. What is the percentage of customers who increase their closing balance by more than 5%?
+
+
+
+
+
+
+
+
+
+
