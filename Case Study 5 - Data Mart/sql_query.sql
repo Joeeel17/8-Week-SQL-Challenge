ac1be94353d2be5434d95042cc09069cebe7211f
Jaime M. Shaker <eyeweld@gmail.com>
2022-08-13 07:55:55 -0500

    Added before and after analysis

--- Case Study 5 - Data Mart/sql_query.sql
+++ Case Study 5 - Data Mart/sql_query.sql
@@ -394,8 +394,41 @@ calendar_year|platform|avg_transaction_size|
          2020|Retail  |                  36|
          2020|Shopify |                 179|
 
+/*
+	Before & After Analysis
+	
+	This technique is usually used when we inspect an important event and want to inspect the 
+	impact before and after a certain point in time.
+	
+	Taking the week_date value of 2020-06-15 as the baseline week where the Data Mart sustainable packaging 
+	changes came into effect.
+
+	We would include all week_date values for 2020-06-15 as the start of the period after the change and 
+	the previous week_date values would be before
+	
+	Since the week of 2020-06-15 is the base, lets find out what week number this is.
+*/    
+         
+SELECT
+	DISTINCT week_number
+FROM
+	clean_weekly_sales
+WHERE 
+	week_day = '2020-06-15'
+	
+-- Results:
+	
+week_number|
+-----------+
+         25|
 
 
+-- 1. What is the total sales for the 4 weeks before and after 2020-06-15? What is the growth or reduction 
+-- rate in actual values and percentage of sales? 
+
+
+	
+        
 
 
 
