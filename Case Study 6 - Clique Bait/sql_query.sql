cca4abe033ab20a6462a582bd2cefdc668240580
Jaime M. Shaker <eyeweld@gmail.com>
2022-09-10 18:22:02 -0500

    Answered question #3

--- Case Study 6 - Clique Bait/sql_query.sql
+++ Case Study 6 - Clique Bait/sql_query.sql
@@ -33,10 +33,48 @@ from
 	GROUP BY
 		user_id) AS tmp
 
+-- Or
+		
+SELECT
+	round(avg(count(DISTINCT cookie_id)) OVER (), 2) AS avg_cookies
+FROM 
+	clique_bait.users
+GROUP BY
+	user_id
+LIMIT 1;
+
 -- Results:
 
 avg_cookies|
 -----------+
        3.56|
+       
+-- 3. What is the unique number of visits by all users per month?
+ 
+SELECT
+	visited_month,
+	sum(n_visits) AS total_visits
+from
+	(SELECT
+		DISTINCT cookie_id,
+		count(DISTINCT visit_id) AS n_visits,
+		extract('month' FROM event_time) AS visited_month
+	FROM clique_bait.events
+	GROUP BY 
+		cookie_id,
+		visited_month) AS tmp
+GROUP BY 
+	visited_month
+ORDER BY visited_month;
+
+-- Results:
+
+visited_month|total_visits|
+-------------+------------+
+          1.0|         876|
+          2.0|        1488|
+          3.0|         916|
+          4.0|         248|
+          5.0|          36|
     
 
