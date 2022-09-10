a82b1146b43baf14655b982c145e3de4386c4e5a
Jaime M. Shaker <eyeweld@gmail.com>
2022-09-10 18:11:38 -0500

    Answered question #1 and #2

--- Case Study 6 - Clique Bait/sql_query.sql
+++ Case Study 6 - Clique Bait/sql_query.sql
@@ -4,3 +4,39 @@
  *  
 */
 
+-- A. Digital Analysis
+
+-- 1. How many users are there?
+
+SELECT 
+	count(DISTINCT user_id) AS n_users
+FROM 
+	clique_bait.users;
+
+-- Results:
+
+n_users|
+-------+
+    500|
+    
+    
+-- How many cookies does each user have on average?
+
+SELECT
+	round(avg(n_cookies), 2) AS avg_cookies
+from
+	(SELECT 
+		DISTINCT user_id,
+		count(DISTINCT cookie_id) AS n_cookies
+	FROM 
+		clique_bait.users
+	GROUP BY
+		user_id) AS tmp
+
+-- Results:
+
+avg_cookies|
+-----------+
+       3.56|
+    
+
