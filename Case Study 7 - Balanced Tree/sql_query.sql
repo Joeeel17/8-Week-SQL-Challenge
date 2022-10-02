de6eda9998c0556470589e0e43114f00b06425f7
Jaime M. Shaker <eyeweld@gmail.com>
2022-10-02 07:56:34 -0500

    Adding High Level Sales Analysis- Adding clarification to question #1

--- Case Study 7 - Balanced Tree/sql_query.sql
+++ Case Study 7 - Balanced Tree/sql_query.sql
@@ -6,14 +6,55 @@
 
 -- A.  High Level Sales Analysis
 
--- 1. What was the total quantity sold for all products?
+-- The first question is asked vaguely.  Although I feel the second answer is the 
+-- correct one, the question is somewhat ambiguous so I added a little clarification. jaime.m.shaker@gmail.com   
+-- 1a. What was the total quantity sold for all products?
 
 SELECT 
-	sum(qty) AS total_quantity
+	sum(qty) AS total_product_quantity
+FROM balanced_tree.sales;
+
+total_product_quantity|
+----------------------+
+                 45216|
+                 
+-- 1b. What was the total quantity sold for EACH product?                 
+
+SELECT 
+	pd.product_name,
+	sum(s.qty) AS total_quantity
+FROM 
+	balanced_tree.sales AS s
+JOIN
+	balanced_tree.product_details AS pd ON pd.product_id = s.prod_id
+GROUP BY
+	pd.product_name;
+
+-- Results:
+
+product_name                    |total_quantity|
+--------------------------------+--------------+
+White Tee Shirt - Mens          |          3800|
+Navy Solid Socks - Mens         |          3792|
+Grey Fashion Jacket - Womens    |          3876|
+Navy Oversized Jeans - Womens   |          3856|
+Pink Fluro Polkadot Socks - Mens|          3770|
+Khaki Suit Jacket - Womens      |          3752|
+Black Straight Jeans - Womens   |          3786|
+White Striped Socks - Mens      |          3655|
+Blue Polo Shirt - Mens          |          3819|
+Indigo Rain Jacket - Womens     |          3757|
+Cream Relaxed Jeans - Womens    |          3707|
+Teal Button Up Shirt - Mens     |          3646|
+         
+-- 2. What is the total generated revenue for all products before discounts?
+         
+SELECT 
+	sum(price) AS gross_revenue
 FROM balanced_tree.sales
 
 -- Results:
 
-total_quantity|
---------------+
-         45216|
+gross_revenue|
+-------------+
+       429290|
