af41d1f777d2170da9219e36d889dcb044505988
Jaime M. Shaker <eyeweld@gmail.com>
2022-10-02 07:26:01 -0500

    Adding High Level Sales Analysis question #1

--- Case Study 7 - Balanced Tree/sql_query.sql
+++ Case Study 7 - Balanced Tree/sql_query.sql
@@ -4,3 +4,16 @@
  *  
 */
 
+-- A.  High Level Sales Analysis
+
+-- 1. What was the total quantity sold for all products?
+
+SELECT 
+	sum(qty) AS total_quantity
+FROM balanced_tree.sales
+
+-- Results:
+
+total_quantity|
+--------------+
+         45216|
